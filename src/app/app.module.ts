3589fe8123a700c2e8d909dfc0583dca92a90664
Piter Velasquez <pealveco@yahoo.com>
2022-08-24 00:57:55 -0500

    Servicio de data creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,14 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,21 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-status: string = "Form no submitted";
+/*status: string = "Form no submitted";
+defaultName: string = "";
+user: string = "";*/
+  products = [];
 
-  constructor() {
+  constructor(private dataService: DataService) {
     /* console.log("Dentro del constructos de app-home"); */
-
   }
+
 
   ngOnInit(): void {
     /* console.log("Dentro del ngOnInit de app-home"); */
-
+    this.dataService.sendGetRequest().subscribe((data: any[]) => {
+      console.log(data);
+      this.products = data;
+    });
   }
 
-  onSendForm(){
-    /* alert("Formulario Enviado"); */
+  /*onSendForm(){
+    //alert("Formulario Enviado");
     this.status = "Form has been send";
-  }
+  }*/
 
+  /*onDefaultForm(){
+    this.defaultName = "Fornite";
+  }*/
 }
